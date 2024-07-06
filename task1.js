4b4db0fd6288e26140aa1ac2b9353a4742199a99
sayanB2004 <168609070+sayanB2004@users.noreply.github.com>
2024-07-06 18:43:19 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,9 @@
+var crsr = document.querySelector("#cursor");
+
+
+document.addEventListener("mousemove", function (dets) {
+  crsr.style.left = dets.x + "px";
+  crsr.style.top = dets.y + "px";
+})
+
+
